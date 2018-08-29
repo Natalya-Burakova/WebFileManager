d4770b8cfe629bb2b96fb51e45479a4559ed7059
Natalya Burakova <n-byrakova@mail.ru>
2018-08-29 18:24:09 +0300

    commit

--- src/main/java/fileManager/config/root/SpringRootConfig.java
+++ src/main/java/fileManager/config/root/SpringRootConfig.java
@@ -2,20 +2,34 @@ package fileManager.config.root;
 
 import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-@ComponentScan({ "fileManager" })
+@ComponentScan({"fileManager"})
 @Configuration
+@ImportResource("classpath:/spring-bean-config.xml")
 public class SpringRootConfig {
 
     @Autowired
     DataSource dataSource;
+
+    @Value("${jdbc.url}")
+    String url;
+
+    @Value("${jdbc.user}")
+    String user;
+
+    @Value("${jdbc.password}")
+    String password;
+
 
     @Bean
     public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
@@ -24,6 +38,6 @@ public class SpringRootConfig {
 
     @PostConstruct
     public void startDBManager() {
-        DatabaseManagerSwing.main(new String[] { "--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", "" });
+        DatabaseManagerSwing.main(new String[] { "--url", url, "--user", user, "--password", password });
     }
 }
